2375e4bf4e8c23c826772bfc7dbb061b79765831
willydekeyser <willy.de.keyser@skynet.be>
2022-12-25 20:34:11 +0100

    Update appication.properties

--- NewInSpringBoot3.0.0/src/main/java/willydekeyser/exception/ExceptionHandlerAdvice.java
+++ NewInSpringBoot3.0.0/src/main/java/willydekeyser/exception/ExceptionHandlerAdvice.java
@@ -5,10 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ExceptionHandlerAdvice extends ResponseEntityExceptionHandler {
+public class ExceptionHandlerAdvice {
 
 	@ExceptionHandler(UnknownUsersException.class)
 	public ProblemDetail handleUnknownUsersException(UnknownUsersException ex) {
